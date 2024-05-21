823478734b52f2a8ee7e4c9b2fafbec14286fa31
Joost Meijles <joost.meijles@avivasolutions.nl>
2024-05-21 10:18:45 +0200

    Use NextResponse

--- app/items/[slug]/route.ts
+++ app/items/[slug]/route.ts
@@ -1,10 +1,12 @@
+import { NextResponse } from "next/server";
+
 export async function GET(
     request: Request,
     { params }: { params: { slug: string } }
 ) {
     const slug = params.slug // 'a', 'b', or 'c'
 
-    return Response.json({ slug })
+    return NextResponse.json({ slug })
 }
 
 export async function generateStaticParams() {

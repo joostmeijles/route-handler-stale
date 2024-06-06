178aed211af2e50f9cf4348b1946b396c952ed7a
Joost Meijles <joost.meijles@avivasolutions.nl>
2024-06-06 16:39:15 +0200

    Add example with dynamic params false

---
+++ app/items-dynamicparams/[slug]/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export const dynamicParams = false;
+
+export async function GET(
+    request: Request,
+    { params }: { params: { slug: string } }
+) {
+    const slug = params.slug // 'a', 'b', or 'c'
+
+    return NextResponse.json({ slug })
+}
+
+export async function generateStaticParams() {
+    return [];
+}

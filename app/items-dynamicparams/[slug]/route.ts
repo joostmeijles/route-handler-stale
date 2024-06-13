9e44e8a1e3510347fa267efd8192d399ff80f66c
Joost Meijles <joost.meijles@avivasolutions.nl>
2024-06-13 11:47:17 +0200

    Use dynamic param

--- app/items-dynamicparams/[slug]/route.ts
+++ app/items-dynamicparams/[slug]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export const dynamicParams = 'force-static';
+export const dynamic = 'force-static';
 
 export async function GET(
     request: Request,
